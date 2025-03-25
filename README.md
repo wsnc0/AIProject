f3ce7de994c60221d0ea2e550f1972651e35f675
Vanboii <144983255+Vanboii@users.noreply.github.com>
2025-03-25 18:06:40 +0800

    New FIles: - .gitignire - dataProcessing.py - test1.ipynb Modified: - README.md

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # AIProject
+
+Project Description
+
+CNN Classifier for dataset: https://www.kaggle.com/datasets/ismailpromus/skin-diseases-image-dataset/data
+
+\Proj Folder
+| - \data
+|   | - Class 0
+|   | - Class 1
+|   | - Class 2
+|   | - Class 3
+|   | - Class 4
+|   | - Class 5
+|   | - Class 6
+|   | - Class 7
+|   | - Class 8
+|   | - Class 9
+| - dataProcessing.py
+| - README.MD
+| - test1.ipynb
+
+Task: Supervised Classification task with a CNN, using Transfer Learning on a EfficientNet Model
+
+Objectives:
+1. DataCleaning/Processing
+    1. DataSet Splitting -> 90 : 10
+    1. For Training Set: 90%
+        1. Possibility of removing Watermarks?
+        1. Padding To Square,
+        1. DownSizing images (to reduce memory requirements)
+        1. Normalizing Channels (for faster Learning)
+    1. For Test Set: 10%
+        1. No Augmentation or anything, use it raw to test for accuracy and generalization
+1. Model Building/Setup
+    1. Importing Model from torchvision
+    1. Freezing of all layers except the classification layer.
+    1. 
+1. Model Training
+    1. Number of Epoch, LR, BatchSize,  
+    1. Progressive Unfreezing of Params for deeper learning of features. 
+1. Model Assessment
+    1. Additional Metrics:
---
+++ dataProcessing.py
@@ -0,0 +1,39 @@
+import os
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+# import numpy as np
+import matplotlib.pyplot as plt
+
+mean = torch.tensor([0.485, 0.456, 0.406])
+std = torch.tensor([0.229, 0.224, 0.225])
+
+
+def pad_to_Square(image):
+    width, height = image.size
+    max_side = max(width,height)
+    left_pad = (max_side-width)//2
+    right_pad = (max_side-width-left_pad)
+    top_pad = (max_side-height)//2
+    bot_pad = (max_side-height-top_pad)
+
+    padded_image = transforms.functional.pad(image, (left_pad, top_pad, right_pad, bot_pad), padding_mode='constant', fill=0)
+    return padded_image
+
+transform = transforms.Compose([
+    transforms.Lambda(pad_to_Square),
+    transforms.Resize((256, 256)),  # Resize images
+    transforms.ToTensor(),  # Convert images to tensor
+    transforms.Normalize(mean=mean, std=std),  # Normalize images
+])
+
+
+path = './Dataset2'
+# Load dataset from directory
+dataset = datasets.ImageFolder(root=path, transform=transform)
+print(dataset, len(dataset))
+# Create a DataLoader
+dataloader_main = DataLoader(dataset, shuffle=True)
+# dataloader_main = DataLoader(dataset, batch_size=len(dataset), shuffle=True)
+print(len(dataloader_main))
+print(dataloader_main)
